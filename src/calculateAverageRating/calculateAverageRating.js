5e1bd753076f21b0221ddce537392cd517235ae5
Roman Apostol <roman@mate.academy>
2019-04-24 10:52:31 +0300

    Update src/calculateAverageRating/calculateAverageRating.js
    Co-Authored-By: ivadimko <vvadimko@gmail.com>

--- src/calculateAverageRating/calculateAverageRating.js
+++ src/calculateAverageRating/calculateAverageRating.js
@@ -3,7 +3,7 @@
 /**
  * Implement calculateAverageRating function:
  *
- * Function takes 5 non-negative natural numbers (one, two, three, four and
+ * Function takes 5 non-negative natural numbers (countOne, countTwo, countThree, countFour and
  * five). Each of numbers represents amount of people rated product with 1, 2,
  * 3, 4 or 5 stars. Function should return average rating of the product.
  *
